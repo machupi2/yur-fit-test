f8455ec3747adedf22573d45508bc3e071cff30e
YasuakiAsano <y.asano@strippers.jp>
2020-07-15 20:41:58 +0900

    SDK 確認

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using YUR.Fit.Core.Models;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI textStart = null;
+    [SerializeField]
+    private TextMeshProUGUI textUpdate = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnYURStartup(){
+         textStart.text = "aaaaa";
+    }
+
+    public void OnYURUpdate(OverlayStatusUpdate obj){
+         textUpdate.text = "aa";
+    }
+}
